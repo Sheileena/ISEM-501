39c55ff2181a1af1ee0a440b4964d2a602cdc087
shalinad <shalinadamani@gmail.com>
2016-08-07 14:57:44 -0400

    Add more pages.

---
+++ ISEM 501/ISEM 501/script/script.js
@@ -0,0 +1,17 @@
+﻿function navigate(where) {
+    if (where == "aboutus")
+        $('.content').load("AboutUs.html");
+    else if (where == "contactus")
+        $('.content').load("ContactUs.html");
+    else if (where == "careers")
+        $('.content').load("Careers.html");
+    else if (where == "services")
+        $('.content').load("Services.html");
+    else if (where == "sitemap")
+        $('.content').load("Sitemap.html");
+    else if (where == "itstaffing")
+        $('.content').load("ITStaffingServices.html");
+    else if (where == "home")
+        window.location.href = "Home.html";
+
+}
